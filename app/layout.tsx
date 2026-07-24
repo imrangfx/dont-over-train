55abd924110754bb8ef4db4d17f6f4c8b93f0281
imrangfx <imran.mgfx@gmail.com>
2026-07-25 00:56:51 +0600

    feat: integrate Tawk.to live chat script for enhanced user support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono, Oxanium } from "next/font/google";
 import Providers from "@/components/Providers";
 import "./globals.css";
@@ -36,6 +37,19 @@ export default function RootLayout({
     >
       <body>
         <Providers>{children}</Providers>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6a63b36c54bb9f1d4deaccc2/1juan9oms';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
